82ee9d0d62583a214c8194df6c1bfbc1115b0058
Douglas Lowder <douglowder@mac.com>
2025-04-14 19:20:26 -0700

    Turn button screen into section list

--- app/expo_ui/ButtonScreen.tsx
+++ app/expo_ui/ButtonScreen.tsx
@@ -1,145 +1,132 @@
 import { Button } from '@expo/ui/Button';
 import * as React from 'react';
-import { Platform, ScrollView, StyleSheet } from 'react-native';
+import { Platform, ScrollView, SectionList, StyleSheet } from 'react-native';
 import { scale } from 'react-native-size-matters';
 
 import { Page, Section } from '@/components/UI/Page';
 
+const sections = [
+  {
+    title: 'Default',
+    data: [{ title: 'Default' }],
+  },
+  {
+    title: 'System Styles',
+    data: [
+      { title: 'Default', variant: 'default' },
+      { title: 'Bordered', variant: 'bordered' },
+      { title: 'Borderless', variant: 'borderless' },
+      {
+        title: 'Bordered Prominent',
+        variant: 'borderedProminent',
+        platforms: ['ios'],
+      },
+      { title: 'Plain', variant: 'plain', platforms: ['ios'] },
+      { title: 'Outlined', variant: 'outlined', platforms: ['android'] },
+      { title: 'Elevated', variant: 'elevated', platforms: ['android'] },
+    ],
+  },
+  { title: 'Disabled', data: [{ title: 'Disabled', disabled: true }] },
+  {
+    title: 'Button Roles',
+    data: [
+      { title: 'Default', role: 'default' },
+      { title: 'Cancel', role: 'cancel' },
+      { title: 'Destructive', role: 'destructive' },
+    ],
+  },
+  {
+    title: 'Button Images',
+    data: [
+      {
+        title: 'Folder',
+        variant: 'bordered',
+        systemImage: { ios: 'folder', android: 'filled.AccountBox' },
+      },
+      {
+        title: 'Tortoise',
+        variant: 'elevated',
+        systemImage: { ios: 'tortoise', android: 'filled.Warning' },
+      },
+      {
+        title: 'Trash',
+        variant: 'borderless',
+        systemImage: { ios: 'trash', android: 'outlined.Delete' },
+        elementColors: { contentColor: '#FF6347' },
+      },
+      {
+        title: 'Heart',
+        systemImage: { ios: 'heart', android: 'outlined.Favorite' },
+      },
+    ],
+  },
+  {
+    title: 'Android Custom Colored Buttons',
+    data: [
+      {
+        title: 'Blue',
+        elementColors: {
+          containerColor: '#007BFF',
+          contentColor: '#FF6347',
+        },
+        platforms: ['android'],
+      },
+
+      {
+        title: 'Red',
+        elementColors: {
+          containerColor: '#FF6347',
+          contentColor: '#007BFF',
+        },
+        platforms: ['android'],
+      },
+    ],
+  },
+  {
+    title: 'Tinted Buttons',
+    data: [{ title: 'Red', color: '#f00f0f' }],
+  },
+];
+
+const renderRow = ({ item }: { item: any }) => {
+  if (item.platforms && !item.platforms.includes(Platform.OS)) {
+    return null;
+  }
+  return (
+    <Button
+      key={item.title}
+      style={styles.button}
+      variant={item.variant}
+      disabled={item.disabled}
+      role={item.role}
+      systemImage={item.systemImage}
+      elementColors={item.elementColors}
+      color={item.color}
+    >
+      {item.title}
+    </Button>
+  );
+};
+
+const renderSectionHeader = ({ section }: { section: { title: string } }) => (
+  <Section title={section.title} />
+);
+
 export default function UIScreen() {
   return (
     <Page>
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <Section title="Default">
-          <Button style={styles.button}>Test</Button>
-        </Section>
-        <Section title="System Styles">
-          <Button style={styles.button} variant="default">
-            Default
-          </Button>
-          <Button style={styles.button} variant="bordered">
-            Bordered
-          </Button>
-          <Button style={styles.button} variant="borderless">
-            Borderless
-          </Button>
-          {Platform.OS === 'ios' && (
-            <>
-              <Button style={styles.button} variant="borderedProminent">
-                Bordered Prominent
-              </Button>
-              <Button style={styles.button} variant="plain">
-                Plain
-              </Button>
-            </>
-          )}
-          {Platform.OS === 'android' && (
-            <>
-              <Button style={styles.button} variant="outlined">
-                Outlined
-              </Button>
-              <Button style={styles.button} variant="elevated">
-                Elevated
-              </Button>
-            </>
-          )}
-        </Section>
-        <Section title="Disabled">
-          <Button style={styles.button} disabled>
-            Disabled
-          </Button>
-          <Button style={styles.button}>Enabled</Button>
-        </Section>
-        {Platform.OS === 'ios' && (
-          <Section title="Button Roles">
-            <Button style={styles.button} role="default">
-              Default
-            </Button>
-            <Button style={styles.button} role="cancel">
-              Cancel
-            </Button>
-            <Button style={styles.button} role="destructive">
-              Destructive
-            </Button>
-          </Section>
-        )}
-        <Section title="Button Images">
-          <Button
-            variant="bordered"
-            style={styles.button}
-            systemImage={{
-              ios: 'folder',
-              android: 'filled.AccountBox',
-            }}
-          >
-            Folder
-          </Button>
-          <Button
-            variant="elevated"
-            style={styles.button}
-            systemImage={{
-              ios: 'tortoise',
-              android: 'filled.Warning',
-            }}
-          >
-            Tortoise
-          </Button>
-          <Button
-            variant="borderless"
-            style={styles.button}
-            systemImage={{
-              ios: 'trash',
-              android: 'outlined.Delete',
-            }}
-            elementColors={{ contentColor: '#FF6347' }}
-          >
-            Trash
-          </Button>
-          <Button
-            variant="outlined"
-            style={styles.button}
-            systemImage={{
-              ios: 'heart',
-              android: 'outlined.Favorite',
-            }}
-          >
-            Heart
-          </Button>
-        </Section>
-        {Platform.OS === 'android' && (
-          <Section title="Android Custom Colored Buttons">
-            <Button
-              style={styles.button}
-              elementColors={{
-                containerColor: '#007BFF',
-                contentColor: '#FF6347',
-              }}
-            >
-              Blue
-            </Button>
-            <Button
-              style={styles.button}
-              elementColors={{
-                containerColor: '#FF6347',
-                contentColor: '#007BFF',
-              }}
-            >
-              Red
-            </Button>
-          </Section>
-        )}
-        <Section title="Tinted Buttons">
-          <Button style={styles.button} color="#f00f0f">
-            Red
-          </Button>
-        </Section>
-      </ScrollView>
+      <SectionList
+        sections={sections}
+        renderItem={renderRow}
+        renderSectionHeader={renderSectionHeader}
+      />
     </Page>
   );
 }
 
 const styles = StyleSheet.create({
   button: {
-    width: scale(150),
+    width: Platform.isTV || Platform.OS === 'web' ? scale(500) : scale(150),
     margin: scale(5),
     overflow: 'visible',
   },
